841d1a88031ddddf9a310bdf4740a54ef971b93f
apmolina <apmolina@iu.edu>
2021-10-05 11:09:35 -0400

    DialogueManager SO CLOSE!

--- 3D Tutorial/Assets/QuizChecker.cs
+++ 3D Tutorial/Assets/QuizChecker.cs
@@ -5,35 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class QuizChecker : MonoBehaviour
 {
-    int _childCount;
+    public static int _childCount;
     [SerializeField] FoodQuiz _quiz1;
     [SerializeField] FoodQuiz _quiz2;
     [SerializeField] FoodQuiz _quiz3;
+
+    [SerializeField] GameObject _canvas;
+    [SerializeField] RuntimeData _runtimeData;
+    public static int count;
+
     // Start is called before the first frame update
     void Start()
     {
         _childCount = gameObject.transform.childCount;
+        count = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        int count = 0;
         if(_quiz1._done == true){
             count++;
+            _quiz1._done = false;
         }
 
         if(_quiz2._done == true){
             count++;
+            _quiz2._done = false;
         }
 
         if(_quiz3._done == true){
             count++;
+            _quiz3._done = false;
         }
 
         if(count == _childCount){
             if(SceneManager.GetActiveScene().name == "SampleScene"){
-                SceneManager.LoadScene(1);
+                _canvas.transform.Find("NextLevelButton").gameObject.SetActive(true);
+                _runtimeData.CurrentGameplayState = GameplayState.InDialog;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            if(SceneManager.GetActiveScene().name == "SceneTwo"){
+                _canvas.transform.Find("You Win").gameObject.SetActive(true);
+                _canvas.transform.Find("ExitButton").gameObject.SetActive(true);
+                _runtimeData.CurrentGameplayState = GameplayState.InDialog;
+                Cursor.lockState = CursorLockMode.None;
             }
         }
     }
